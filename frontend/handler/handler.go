308137cfad03c23207b5e39680c74b5d805dd261
sadovnikovdaniil2 <sadovnikovdaniil2@gmail.com>
2025-02-16 01:51:17 +0300

    remove commited rows

--- frontend/handler/handler.go
+++ frontend/handler/handler.go
@@ -43,48 +43,35 @@ func (h *ChatHandler) GetRoomsLastMessage(c *gin.Context) (int, map[string]strin
 	if err != nil {
 		h.logger.Error("error reading directory with messages", zap.Error(err))
 		return http.StatusInternalServerError, nil, errors.Wrap(err, "error reading directory with messages")
-		// c.String(http.StatusInternalServerError, fmt.Errorf("error reading directory with messages: %s", err).Error())
-		// return
 	}
 
 	rooms := h.iterateByMessageFiles(files)
 	return http.StatusOK, rooms, errors.Wrap(err, "error iterate by messages")
-	// c.JSON(http.StatusOK, rooms)
 }
 
 func (h *ChatHandler) GetMessagesFile(c *gin.Context) (int, string, error) {
 	room := c.Query("room")
 	if room == "" {
 		return http.StatusBadRequest, "", errors.New("empty room")
-		// c.JSON(http.StatusBadRequest, gin.H{"error": "missing room"})
-		// return
 	}
 
 	logFilePath := fmt.Sprintf("./messages/%s.log", room)
 	if _, err := os.Stat(logFilePath); err != nil {
 		return http.StatusNotFound, "", errors.Wrap(err, "room log file not found")
-		// c.JSON(http.StatusNotFound, gin.H{"error": fmt.Errorf("room log file not found: %q", err).Error()})
-		// return
 	}
-
 	return http.StatusOK, logFilePath, nil
-	// c.File(logFilePath)
 }
 
 func (h *ChatHandler) SendMessage(c *gin.Context) (int, error) {
 	var request models.MessageRequest
 	if err := c.BindJSON(&request); err != nil {
 		h.logger.Error("failed to bind json request", zap.Error(err))
-		// c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Errorf("invalid request: %s", err).Error()})
 		return http.StatusBadRequest, errors.Wrap(err, "invalid request")
 	}
 
 	if !request.Validate() {
 		h.logger.Error("failed to validate request", zap.Any("request", request))
 		return http.StatusBadRequest, errors.New("missing room, nick or message")
-
-		// c.JSON(http.StatusBadRequest, gin.H{"error": "missing room, nick, or message"})
-		// return
 	}
 
 	message, err := request.ToMessage()
--- frontend/handler/handler_view.go
+++ frontend/handler/handler_view.go
@@ -17,25 +17,19 @@ func (h *ChatHandler) GetRoomView(c *gin.Context) (int, []byte, error) {
 	if room == "" || nick == "" {
 		h.logger.Error("empty room or nick in request", zap.String("room", room), zap.String("nick", nick))
 		return http.StatusBadRequest, nil, errors.New("missing room or nick")
-		// c.JSON(http.StatusBadRequest, gin.H{"error": errors.New("missing room or nick").Error()})
 	}
 
 	if err := h.api.JoinRoom(c, room, nick); err != nil {
 		return http.StatusInternalServerError, nil, err
-		// c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		// return
 	}
 
 	data, err := content.ReadFile("room.html")
 	if err != nil {
 		h.logger.Error("can not read room.html", zap.Error(err))
 		return http.StatusInternalServerError, nil, err
-		// c.String(http.StatusInternalServerError, err.Error())
-		// return
 	}
 
 	return http.StatusOK, data, nil
-	// c.Data(http.StatusOK, "text/html; charset=utf-8", data)
 }
 
 func (h *ChatHandler) GetRoomsListView(c *gin.Context) (int, []byte, error) {
@@ -43,12 +37,9 @@ func (h *ChatHandler) GetRoomsListView(c *gin.Context) (int, []byte, error) {
 	if err != nil {
 		h.logger.Error("can not read room_list.html", zap.Error(err))
 		return http.StatusInternalServerError, nil, err
-		// c.String(http.StatusInternalServerError, err.Error())
-		// return
 	}
 
 	return http.StatusOK, data, nil
-	// c.Data(http.StatusOK, "text/html; charset=utf-8", data)
 }
 
 func (h *ChatHandler) GetAuthView(c *gin.Context) (int, []byte, error) {
@@ -56,10 +47,7 @@ func (h *ChatHandler) GetAuthView(c *gin.Context) (int, []byte, error) {
 	if err != nil {
 		h.logger.Error("can not read login.html", zap.Error(err))
 		return http.StatusInternalServerError, nil, err
-		// c.String(http.StatusInternalServerError, err.Error())
-		// return
 	}
 
 	return http.StatusOK, data, nil
-	// c.Data(http.StatusOK, "text/html; charset=utf-8", data)
 }
