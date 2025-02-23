c01372c7d3aee10b29cb924453216d46a5b0dc7c
sadovnikovdaniil2 <sadovnikovdaniil2@gmail.com>
2025-02-24 00:42:58 +0300

    add image uploading

--- models/room.go
+++ models/room.go
@@ -2,14 +2,21 @@ package models
 
 import (
 	"encoding/base64"
+	"errors"
+)
 
-	"github.com/pkg/errors"
+type MsgType string
+
+const (
+	Image MsgType = "image"
+	Text  MsgType = "text"
 )
 
 type MessageRequest struct {
-	Room    string `json:"room"`
-	Nick    string `json:"nick"`
-	Message string `json:"base64Message"`
+	Type    MsgType `json:"type"`
+	Room    string  `json:"room"`
+	Nick    string  `json:"nick"`
+	Message []byte  `json:"base64Message"`
 }
 
 func (r *MessageRequest) Validate() bool {
@@ -17,12 +24,15 @@ func (r *MessageRequest) Validate() bool {
 }
 
 func (m *MessageRequest) ToMessage() (Message, error) {
-	data, err := base64.StdEncoding.DecodeString(m.Message)
-	if err != nil {
-		return Message{}, errors.Wrap(err, "failed to convert string base64 message to byte array")
+	switch m.Type {
+	case Image:
+		data := base64.StdEncoding.EncodeToString(m.Message)
+		return Message{Room: m.Room, Nick: m.Nick, Message: []byte(data)}, nil
+	case Text:
+		return Message{Room: m.Room, Nick: m.Nick, Message: m.Message}, nil
+	default:
+		return Message{}, errors.New("invalid message type")
 	}
-
-	return Message{Room: m.Room, Nick: m.Nick, Message: data}, nil
 }
 
 type Message struct {
